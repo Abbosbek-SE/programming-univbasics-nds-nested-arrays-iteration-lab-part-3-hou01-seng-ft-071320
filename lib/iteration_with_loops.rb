f523637c5cfa96750e7f5d1741a6b7f3e4c34919
Abbosbek Muydinboev <muydinboevabbosbek@gmail.com>
2020-07-01 06:40:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,15 @@
 def join_nested_strings(src)
-   string = ""
-   i = 0
-   while i < src.length do 
-     j = 0 
-     while j < src[i].length do
-       if src[i][j] == ""
-         string += src[i][j]
-       end
-       j += 1
-     end
-     i += 1
-   end
-   return string
+  string = ""
+  i = 0 
+  while i < src.length do
+    j = 0 
+    while j < src[i].length do
+      if (src[i][j]).class == String 
+        string += " " + src[i][j]
+      end
+      j += 1 
+    end
+    i += 1
+  end
+  return string
 end
